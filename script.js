b7982a2c5172ad5fc130484fa1dca863c5d6a4bb
HARINIGOUD28 <138192848+HARINIGOUD28@users.noreply.github.com>
2023-11-17 19:02:09 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const apikey = "adc569fa33627e6d5ba1fa3a62009e16;
+const apikey = "d17502c57141eb3f2a8b6b17df8b0";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
